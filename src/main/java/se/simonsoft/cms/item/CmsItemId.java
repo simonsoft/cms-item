0b9f6e79b3b43d1ea16245386c07231e1be33527
Staffan Olsson <staffan@repos.se>
2011-01-17 16:01:03 +0000

    Renamed to comply with SvnLogicalId tradition. Requires some changes to existing implementations in SvnLogicalId, for example getRelPath should now be decoded.

--- src/main/java/se/simonsoft/cms/item/CmsItemId.java
+++ src/main/java/se/simonsoft/cms/item/CmsItemId.java
@@ -9,8 +9,9 @@ public interface CmsItemId {
 	
 	/**
 	 * Resource URL, no query string. Not even revision number.
-	 * If revision ({@link #getRevision()}) is specified this is the URL at that revision (i.e. "peg").
-	 * @return decoded resource URL, i.e. might contain whitespace
+	 * If revision ({@link #getPegRev()}) is specified this is the URL at that revision (i.e. "peg").
+	 * @return resource URL, encoded, encoding based on UTF-8 bytes for non-ascii
+	 * TODO encoded instead
 	 * TODO how about http/https?
 	 */
 	String getUrl();
@@ -19,7 +20,7 @@ public interface CmsItemId {
 	 * The exact revision, if specified, i.e. never HEAD or date or anything but the integer.
 	 * @return revision number, or null for revision not specified (HEAD in subversion terminology)
 	 */
-	Long getRevision();
+	Long getPegRev();
 	
 	/**
 	 * Local id without hostname, unique within the context of a connection to a server.
@@ -38,10 +39,10 @@ public interface CmsItemId {
 	/**
 	 * @return path in repository, decoded, including leading slash
 	 */
-	String getPath();
+	String getRelPath();
 	
 	/**
-	 * @return repository root URL without trailing slash
+	 * @return repository root URL without trailing slash, encoded
 	 */
 	String getRepositoryUrl();
 	
