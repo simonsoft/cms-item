edb232bd3205cc70b2b16026b69bfc5e3bab679f
Staffan Olsson <staffan@repos.se>
2011-02-10 20:57:16 +0000

    Upgraded to parent 1.4. Added base class for CmsItem implementations. Can be used to reduce code maintenance as the API grows, for implementations that are not intended to support the full CMS item model.

---
+++ src/main/java/se/simonsoft/cms/item/CmsItemBase.java
@@ -0,0 +1,21 @@
+package se.simonsoft.cms.item;
+
+/**
+ * Optional base class for {@link CmsItem}, throwing {@link UnsupportedOperationException} in all methods.
+ * 
+ * Extended by implementations that don't want compilation errors as new methods are added.
+ * Also useful for anonymous inner classes in unit tests.
+ */
+public abstract class CmsItemBase implements CmsItem {
+
+	@Override
+	public CmsItemId getId() {
+		throw new UnsupportedOperationException("getId not implemented");
+	}
+
+	@Override
+	public String getStatus() {
+		throw new UnsupportedOperationException("getStatus not implemented");
+	}
+
+}
