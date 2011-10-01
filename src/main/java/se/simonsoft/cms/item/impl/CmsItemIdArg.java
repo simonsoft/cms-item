41914fac0da7ae2c02542ff2d5760fd702512d9a
Staffan Olsson <staffan@repos.se>
2011-10-01 06:29:03 +0000

    Completed the logical-id-as-string-argument class. Hostname could be added eithre by repos, the REST server itself, or some clever configuration in the webapp using AOP or pluggable string to type converters.

--- src/main/java/se/simonsoft/cms/item/impl/CmsItemIdArg.java
+++ src/main/java/se/simonsoft/cms/item/impl/CmsItemIdArg.java
@@ -1,29 +1,65 @@
 package se.simonsoft.cms.item.impl;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import se.simonsoft.cms.item.CmsItemId;
 import se.simonsoft.cms.item.CmsItemPath;
 
 /**
  * Concrete class for use in the common case when a logical id
  * is provided as string argument to an operation.
+ * 
  * Mutable: allows adding of hostname and peg revision if not already set.
+ * 
  * Provides a String argument constructor for use as JAX-RS parameter.
+ * 
+ * This class never needs to deal with encoding of logical ID, which is tricky,
+ * only decoding of path, which is simple. Assuming hostname is always ascii.
  */
 public class CmsItemIdArg implements CmsItemId {
 
+	public static final String PROTO = "x-svn://";
+	public static final String HTTP = "http://";
+	public static final String PEG = "?p=";
+	public static final Pattern NICE = Pattern.compile(PROTO + "([^/]*)(.*/)([^^]*)\\^([^?]+)(?:\\?p=(\\d+))?");
+	private String host = "";
+	private String parent;
+	private String repo;
+	private String relpath;
+	private Long pegRev = null;
+	private boolean orgfull;
+	private boolean orgpeg;
+	
 	/**
 	 * @param logicalId with or without hostname and peg rev
 	 */
 	public CmsItemIdArg(String logicalId) {
-		
+		Matcher m = NICE.matcher(logicalId);
+		if (!m.matches()) {
+			throw new IllegalArgumentException("Not a valid logical id: " + logicalId);
+		}
+		this.host = m.group(1);
+		this.parent = m.group(2);
+		this.repo = m.group(3);
+		this.relpath = m.group(4);
+		if (m.group(5) != null) {
+			this.pegRev  = Long.parseLong(m.group(5));
+			this.orgpeg = true;
+		}
+		if (isFullyQualified()) {
+			this.orgfull = true;
+		}
 	}
 	
 	public boolean isFullyQualified() {
-		throw new UnsupportedOperationException("Method not implemented");
+		return host.length() > 0;
 	}
 	
 	public boolean isFullyQualifiedOriginally() {
-		throw new UnsupportedOperationException("Method not implemented");
+		return orgfull;
 	}
 	
 	/**
@@ -31,11 +67,11 @@ public class CmsItemIdArg implements CmsItemId {
 	 * @return true if id has peg rev
 	 */
 	public boolean isPegged() {
-		throw new UnsupportedOperationException("Method not implemented");
+		return pegRev != null;
 	}
 	
 	public boolean isPeggedOriginally() {
-		throw new UnsupportedOperationException("Method not implemented");
+		return orgpeg;
 	}
 	
 	/**
@@ -43,11 +79,40 @@ public class CmsItemIdArg implements CmsItemId {
 	 * when the caller is on the same server.
 	 */
 	public void setHostname(String fullyQualifiedName) {
-		
+		if (isFullyQualified()) {
+			throw new IllegalStateException("Hostname already set for " + getLogicalIdFull());
+		}
+		if (fullyQualifiedName == null || fullyQualifiedName.length() == 0) {
+			throw new IllegalArgumentException("Method not intended for removing hostname");
+		}
+		host = fullyQualifiedName;
 	}
 	
+	/**
+	 * Makes sure hostname is set and matching a given value.
+	 * @param fullyQualifiedName hostname to set or to expect
+	 * @throws IllegalArgumentException If the hostname of the logicalId does not match the given
+	 */
+	public void setHostnameOrValidate(String fullyQualifiedName)
+			throws IllegalArgumentException {
+		if (isFullyQualified()) {
+			if (!host.equals(fullyQualifiedName))  {
+				throw new IllegalArgumentException("Unexpected hostname in " + getLogicalIdFull() + ", expected " + fullyQualifiedName);
+			}
+		} else {
+			setHostname(fullyQualifiedName);
+		}
+	}
+	
+	/**
+	 * @param pegRev when given as an optional parameter alongside the id
+	 */
 	public void setPegRev(long pegRev) {
-		
+		if (isPegged()) {
+			throw new IllegalStateException("Peg revision already set for " + getLogicalIdPath());
+		} else {
+			this.pegRev = pegRev;
+		}
 	}
 	
 	/**
@@ -56,38 +121,58 @@ public class CmsItemIdArg implements CmsItemId {
 	 */
 	@Override
 	public String getRepositoryUrl() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Method not implemented");
+		if (!isFullyQualified()) {
+			throw new IllegalStateException("Hostname unknown for " + getLogicalId());
+		}
+		return HTTP + host + parent + repo;
+	}
+
+	private String getQuery() {
+		if (isPegged()) {
+			return PEG + pegRev;
+		}
+		return "";
+	}
+
+	private String getLogicalIdPath() {
+		return PROTO + parent + repo + "^" + relpath;
 	}
 	
 	@Override
 	public String getUrl() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Method not implemented");
+		return getRepositoryUrl() + relpath;
 	}
 
 	@Override
 	public Long getPegRev() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Method not implemented");
+		return pegRev;
 	}
 
+	/**
+	 * Always known in this implementation, unlike {@link #getLogicalIdFull()}.
+	 */
 	@Override
 	public String getLogicalId() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Method not implemented");
+		return getLogicalIdPath() + getQuery();
 	}
 
 	@Override
 	public String getLogicalIdFull() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Method not implemented");
+		if (!isFullyQualified()) {
+			throw new IllegalStateException("Hostname unknown for " + getLogicalId());
+		}
+		return PROTO + host + parent + repo + "^" + relpath + getQuery();
 	}
 
 	@Override
 	public CmsItemPath getRelPath() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Method not implemented");
+		String decoded;
+		try {
+			decoded = URLDecoder.decode(relpath, "UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			throw new RuntimeException("Failed to decode with charset UTF-8");
+		}
+		return new CmsItemPath(decoded);
 	}
 
 }
--- src/test/java/se/simonsoft/cms/item/impl/CmsItemIdArgTest.java
+++ src/test/java/se/simonsoft/cms/item/impl/CmsItemIdArgTest.java
@@ -7,28 +7,39 @@ import org.junit.Test;
 
 public class CmsItemIdArgTest {
 	
-	@Test
+	@Test(expected=IllegalArgumentException.class)
+	public void testValidateNoRoot() {
+		new CmsItemIdArg("x-svn:///svn/demo1/vvab/graphics/0001.tif");
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
 	@Ignore
+	public void testValidateNoStartSlash() {
+		new CmsItemIdArg("x-svn://svn/demo1^/vvab/graphics/0001.tif");
+	}
+	
+	@Test
 	public void testPegHost() {
 		CmsItemIdArg p = new CmsItemIdArg("x-svn://demo.simonsoftcms.se/svn/demo1^/vvab/xml/Docs/Sa%20s.xml?p=9");
 		assertTrue(p.isFullyQualified());
 		assertTrue(p.isFullyQualifiedOriginally());
 		assertTrue(p.isPegged());
 		assertTrue(p.isPeggedOriginally());
+		assertEquals("/vvab/xml/Docs/Sa s.xml", p.getRelPath().toString());
 	}
 	
 	@Test
-	@Ignore
 	public void testPegNoHost() {
 		CmsItemIdArg p = new CmsItemIdArg("x-svn:///svn/demo1^/vvab/xml/sections/In%20in.xml?p=101");
 		assertFalse(p.isFullyQualified());
 		assertFalse(p.isFullyQualifiedOriginally());
 		assertTrue(p.isPegged());
 		assertTrue(p.isPeggedOriginally());
+		p.setHostnameOrValidate("example.net");
+		assertEquals("http://example.net/svn/demo1/vvab/xml/sections/In%20in.xml", p.getUrl());
 	}
 	
 	@Test
-	@Ignore
 	public void testNoHostNoPeg() {
 		CmsItemIdArg p = new CmsItemIdArg("x-svn:///svn/demo1^/vvab/graphics/0001.tif");
 		assertFalse(p.isFullyQualified());
@@ -51,7 +62,7 @@ public class CmsItemIdArgTest {
 		}
 		try {
 			p.getUrl();
-		} catch (IllegalArgumentException e) {
+		} catch (IllegalStateException e) {
 			assertEquals("Hostname unknown for " + p.getLogicalId(), e.getMessage());
 		}
 		p.setHostname("x.y.z");
@@ -62,14 +73,29 @@ public class CmsItemIdArgTest {
 		assertEquals("http://x.y.z/svn/demo1/vvab/graphics/0001.tif", p.getUrl());
 		assertEquals("http://x.y.z/svn/demo1", p.getRepositoryUrl());
 		assertFalse(p.isPegged());
+		assertEquals("API spec", null, p.getPegRev());
 		p.setPegRev(1234567);
 		assertTrue(p.isPegged());
 		assertFalse(p.isPeggedOriginally());
 		assertEquals(1234567, p.getPegRev().longValue());
-		assertEquals("x-svn:///svn/demo1^/vvab/graphics/0001.tif?p=123456", p.getLogicalId());
-		assertEquals("x-svn://x.y.z/svn/demo1^/vvab/graphics/0001.tif?p=123456", p.getLogicalIdFull());
+		assertEquals("x-svn:///svn/demo1^/vvab/graphics/0001.tif?p=1234567", p.getLogicalId());
+		assertEquals("x-svn://x.y.z/svn/demo1^/vvab/graphics/0001.tif?p=1234567", p.getLogicalIdFull());
 		assertEquals("http://x.y.z/svn/demo1/vvab/graphics/0001.tif", p.getUrl());
-		assertEquals("http://x.y.z/svn/demo1", p.getRepositoryUrl());		
+		assertEquals("http://x.y.z/svn/demo1", p.getRepositoryUrl());
+		try {
+			p.setHostname("x.y.z");
+			fail("Expected exception");
+		} catch (IllegalStateException e) {
+			assertEquals("Hostname already set for " + p.getLogicalIdFull(), e.getMessage());
+		}
+		p.setHostnameOrValidate("x.y.z");
+		try {
+			p.setHostnameOrValidate("xx.y.z");
+			fail("Expected exception");
+		} catch (IllegalArgumentException e) {
+			assertEquals("Unexpected hostname in x-svn://x.y.z/svn/demo1^/vvab/graphics/0001.tif?p=1234567, expected xx.y.z",
+					e.getMessage());
+		}
 	}
 
 }
