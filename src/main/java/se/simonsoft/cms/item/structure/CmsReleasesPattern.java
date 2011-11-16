05d2033cafc84c7f63bc1f6eecb5459dcabdf234
Staffan Olsson <staffan@repos.se>
2011-11-16 12:17:29 +0000

    Commented how profiles may affect release APIs. Our preliminary strategy is to force top.level documents that have a profile to be released with that profile, meaning that we preserve the one-to-one relation between master and release.

--- src/main/java/se/simonsoft/cms/item/structure/CmsReleasesPattern.java
+++ src/main/java/se/simonsoft/cms/item/structure/CmsReleasesPattern.java
@@ -1,5 +1,7 @@
 package se.simonsoft.cms.item.structure;
 
+import se.simonsoft.cms.item.CmsItemPath;
+
 /**
  * Used to mark intent when calling this low level API,
  * for future refactoring and addition of rules.
@@ -10,6 +12,20 @@ public class CmsReleasesPattern extends CmsAreaPattern {
 	public CmsReleasesPattern(String configValue)
 			throws IllegalArgumentException {
 		super(configValue);
+	}
+
+	/**
+	 * See {@link CmsAreaPattern#getPathInside(CmsItemPath, String)} but
+	 * note that this can <em>not</em> be used to predict the release path
+	 * because profiling might affect the destination file name.
+	 * 
+	 * Currently the support for transforming with profile info is added in
+	 * adapter code.
+	 */
+	@Override
+	public CmsItemPath getPathInside(CmsItemPath master, String destinationLabel) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Method not implemented");
 	}
 
 }
