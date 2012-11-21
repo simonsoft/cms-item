a25576b92010cd18c61759a3bd2dd34cd4140639
Staffan Olsson <staffan@repos.se>
2012-11-21 14:34:33 +0000

    Think I found a solution to browsing by returning CmsItem in getImmediates instead of CmsItemId, so you can get item kind, while using getImmediateFolders for example when listing release labels or getImmediateFiles for example when listing the graphics in a folder (for batch prepare translation).

--- src/main/java/se/simonsoft/cms/item/info/CmsItemLookup.java
+++ src/main/java/se/simonsoft/cms/item/info/CmsItemLookup.java
@@ -26,7 +26,7 @@ import se.simonsoft.cms.item.CmsItemId;
  * Implementations can be either cross-repository, requiring paths/URLs/logicalIds with repo specified,
  * or single-repo throwing exceptions if repo is specified and not identical to the connected repo.
  */
-@SuppressWarnings("deprecation") // remove for 2.1
+@SuppressWarnings("deprecation") // remove original CmsItemLookup in for 2.2
 public interface CmsItemLookup extends se.simonsoft.cms.item.CmsItemLookup {
 
 	/**
@@ -37,24 +37,43 @@ public interface CmsItemLookup extends se.simonsoft.cms.item.CmsItemLookup {
 	 */	
 	CmsItem getItem(CmsItemId id)
 		throws CmsConnectionException, CmsItemNotFoundException;
-	
-	/**
-	 * @param parent folder, optional peg rev
-	 * @return children, iteration order decided by backend
-	 * @throws CmsConnectionException if connection to the server or repository failed
-	 * @throws CmsItemNotFoundException if the item could not be found
-	 */
-	Set<CmsItemId> getImmediates(CmsItemId parent)
-		throws CmsConnectionException, CmsItemNotFoundException;
 
 	/**
-	 * @param parent folder, optional peg rev
+	 * Typically used from services, while {@link #getImmediates(CmsItemId)} is used for browsing.
+	 * @param parent folder or repository root, optional peg rev
 	 * @return children that are folders, iteration order decided by backend
 	 * @throws CmsConnectionException if connection to the server or repository failed
 	 * @throws CmsItemNotFoundException if the item could not be found
 	 */	
 	Set<CmsItemId> getImmediateFolders(CmsItemId parent)
 		throws CmsConnectionException, CmsItemNotFoundException;
+
+	/**
+	 * Typically used from services, while {@link #getImmediates(CmsItemId)} is used for browsing.
+	 * Symlinks, if supported by backend, should not be included.
+	 * @param parent folder or repository root, optional peg rev
+	 * @return children that are files, iteration order decided by backend
+	 * @throws CmsConnectionException if connection to the server or repository failed
+	 * @throws CmsItemNotFoundException if the item could not be found
+	 */
+	Set<CmsItemId> getImmediateFiles(CmsItemId parent)
+			throws CmsConnectionException, CmsItemNotFoundException;
+	
+	/**
+	 * For directory listing, when you want item kind, size and maybe properties.
+	 * 
+	 * Potentially slower than {@link #getImmediateFiles(CmsItemId)} and {@link #getImmediateFolders(CmsItemId)},
+	 * but should do lazy fetching of per-item data such as properties.
+	 * Backends often deliver directory listings including kind, size and modification date in one call.
+	 * Checksums might be included for some backends but slow for others.
+	 * 
+	 * @param parent folder or repository root, optional peg rev
+	 * @return children, iteration order decided by backend
+	 * @throws CmsConnectionException if connection to the server or repository failed
+	 * @throws CmsItemNotFoundException if the item could not be found
+	 */
+	Set<CmsItem> getImmediates(CmsItemId parent)
+		throws CmsConnectionException, CmsItemNotFoundException;	
 	
 	/**
 	 * Get contents recursively. This is an expensive operation that should be used with care,
