10d762c70fb1c1c073e405f5ff228af929f97be6
Thomas Åkesson <thomas.akesson@simonsoft.se>
2014-02-17 17:32:35 +0000

    #670 Moving the indexing IdStrategy to cms-item in order to facilitate searching based on this ID. These IDs are ephemeral and should never be stored anywhere but in the indexing. Changing IdStrategy can be done by updating the code and reindexing.

---
+++ src/main/java/se/simonsoft/cms/item/indexing/IdStrategy.java
@@ -0,0 +1,69 @@
+/**
+ * Copyright (C) 2004-2012 Repos Mjukvara AB
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package se.simonsoft.cms.item.indexing;
+
+import se.simonsoft.cms.item.CmsItemId;
+import se.simonsoft.cms.item.CmsItemPath;
+import se.simonsoft.cms.item.CmsRepository;
+import se.simonsoft.cms.item.RepoRevision;
+
+/**
+ * Produces different strings to identify repository contents such as files, folders, repositories and commits.
+ */
+public interface IdStrategy {
+
+	/**
+	 * @return full id for an index item such as a file or folder
+	 */
+	public String getId(CmsRepository repository, RepoRevision revision, CmsItemPath path);
+
+	/**
+	 * @return idhead field value
+	 */
+	public String getIdHead(CmsRepository repository, CmsItemPath path);
+	
+	/**
+	 * @param revision needed because itemId does not contain a full revision
+	 * @return same as {@link #getId(CmsRepository, RepoRevision, CmsItemPath)}
+	 */
+	public String getId(CmsItemId itemId, RepoRevision revision);
+	
+	/**
+	 * @return same as {@link #getIdHead(CmsRepository, CmsItemPath)}
+	 */
+	public String getIdHead(CmsItemId itemId);
+
+	/**
+	 * @return prefix to both {@link #getIdCommit(CmsRepository, RepoRevision)} and {@link #getId(CmsItemId)}
+	 */
+	public String getIdRepository(CmsRepository repository);
+	
+	/**
+	 * @return id for item that represents commit, i.e. indexing status and revprops
+	 */
+	public String getIdCommit(CmsRepository repository, RepoRevision revision);
+	
+	/**
+	 * @return id for storing a piece of repository-related information
+	 */
+	public String getIdEntry(CmsRepository repository, String repositoryField);
+
+	/**
+	 * @return the format of a revision
+	 */
+	public String getIdRevision(RepoRevision revision);
+	
+}
---
+++ src/main/java/se/simonsoft/cms/item/indexing/IdStrategyDefault.java
@@ -0,0 +1,109 @@
+/**
+ * Copyright (C) 2004-2012 Repos Mjukvara AB
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package se.simonsoft.cms.item.indexing;
+
+import se.simonsoft.cms.item.CmsItemId;
+import se.simonsoft.cms.item.CmsItemPath;
+import se.simonsoft.cms.item.CmsRepository;
+import se.simonsoft.cms.item.RepoRevision;
+import se.simonsoft.cms.item.impl.CmsItemIdUrl;
+
+public class IdStrategyDefault implements IdStrategy {
+
+	/**
+	 * @return defaults to empty string because paths have no trailing slash
+	 */
+	protected String getRootPath() {
+		return "";
+	}
+	
+	/**
+	 * @return what's inbetween the idhead and the revision identifier
+	 */
+	protected String getPegSeparator() {
+		return "@";
+	}
+	
+	protected String getInfoSeparator() {
+		return "#";
+	}
+	
+	/**
+	 * @param itemId
+	 * @return full revision from itemId, which currently has only the number
+	 */
+	protected RepoRevision getRevision(CmsItemId itemId) {
+		if (itemId.getPegRev() == null) {
+			throw new IllegalArgumentException("Item must have revision to be valid as ID");
+		}
+		return new RepoRevision(itemId.getPegRev(), null);
+	}
+	
+	/**
+	 * @param revision
+	 * @return how to represent a revision in id
+	 */
+	@Override
+	public String getIdRevision(RepoRevision revision) {
+		return Long.toString(revision.getNumber());
+	}
+	
+	protected String getIdHost(CmsRepository repository) {
+		return repository.getHost();
+	}
+	
+	@Override
+	public String getIdRepository(CmsRepository repository) {
+		return getIdHost(repository) + repository.getUrlAtHost();
+	}
+	
+	@Override
+	public String getId(CmsItemId itemId, RepoRevision revision) {
+		if (revision == null) {
+			throw new IllegalArgumentException("Use getIdHead for id without revision");
+		}
+		if (itemId.getPegRev() != null && !revision.equals(new RepoRevision(itemId.getPegRev(), null))) {
+			throw new IllegalArgumentException("Provided revision " + revision + " does not match id "+ itemId);
+		}
+		return getIdHead(itemId) + getPegSeparator() + getIdRevision(revision); 
+	}
+	
+	@Override
+	public String getIdHead(CmsItemId itemId) {
+		return itemId.getRepository().getHost() + itemId.getUrlAtHost();
+	}
+	
+	@Override
+	public String getId(CmsRepository repository, RepoRevision revision, CmsItemPath path) {
+		return getId(new CmsItemIdUrl(repository, path), revision); 
+	}
+
+	@Override
+	public String getIdHead(CmsRepository repository, CmsItemPath path) {
+		return getIdHead(new CmsItemIdUrl(repository, path));
+	}
+	
+	@Override
+	public String getIdCommit(CmsRepository repository, RepoRevision revision) {
+		return getIdEntry(repository, getIdRevision(revision));
+	}
+
+	@Override
+	public String getIdEntry(CmsRepository repository, String repositoryField) {
+		return getIdRepository(repository) + getInfoSeparator() + repositoryField;
+	}	
+	
+}
