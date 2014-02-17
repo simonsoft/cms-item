4d8b95aedc3e3bc818b704bf93077f62f7488fe8
Thomas Åkesson <thomas.akesson@simonsoft.se>
2014-02-17 17:45:00 +0000

    #670 Moving the indexing IdStrategy to cms-item from repos-indexing. Updating copyright.

--- src/main/java/se/simonsoft/cms/item/indexing/IdStrategy.java
+++ src/main/java/se/simonsoft/cms/item/indexing/IdStrategy.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2004-2012 Repos Mjukvara AB
+ * Copyright (C) 2009-2013 Simonsoft Nordic AB
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
