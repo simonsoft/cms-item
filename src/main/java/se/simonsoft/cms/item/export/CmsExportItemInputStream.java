bf8044051718d4e7559db2902da321ca90e05f90
Jonathan Andersson <jonathan.andersson@simonsoft.se>
2017-02-15 11:07:57 +0100

    New export item for InputStreams

---
+++ src/main/java/se/simonsoft/cms/item/export/CmsExportItemInputStream.java
@@ -0,0 +1,88 @@
+/**
+ * Copyright (C) 2009-2016 Simonsoft Nordic AB
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package se.simonsoft.cms.item.export;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class CmsExportItemInputStream implements CmsExportItem {
+
+    private final InputStream inputStream;
+    private final CmsExportPath exportPath;
+
+    private Boolean ready = false;
+    private Logger logger = LoggerFactory.getLogger(CmsExportItemInputStream.class);
+
+    public CmsExportItemInputStream(InputStream inputStream, CmsExportPath exportPath) {
+
+        if (inputStream == null) {
+            throw new IllegalArgumentException("Can't prepare null InputStream for export");
+        }
+
+        if (exportPath == null) {
+            throw new IllegalArgumentException("The export path can not be null");
+        }
+
+        this.inputStream = inputStream;
+        this.exportPath = exportPath;
+
+    }
+
+
+    @Override
+    public void prepare() {
+
+        if (isReady()) {
+            throw new IllegalStateException("InputStream is already prepared for export");
+        }
+
+        logger.info("Starting preparation for export of InputStream");
+        this.ready = true;
+    }
+
+    @Override
+    public Boolean isReady() {
+        return this.ready;
+    }
+
+    @Override
+    public void getResultStream(OutputStream stream) {
+
+        byte[] buffer = new byte[1024];
+        int length;
+        final InputStream content = this.inputStream;
+        try {
+            while ((length = content.read(buffer)) != -1) {
+                stream.write(buffer, 0, length);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Could not read InputStream", e);
+        }
+    }
+
+    @Override
+    public CmsExportPath getResultPath() {
+        return this.exportPath;
+    }
+
+    //TODO: Skall vara ett exportitem som tar en input stream. Skall användas i testet för reconcile review.
+    // Vi behöver ladda upp en string som blir fil så att den sedan kan hämtas.
+
+}
