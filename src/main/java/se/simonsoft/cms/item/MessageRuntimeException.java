bc540035c809be988a5cbe3cc23456e0ccc7a2d0
Thomas Åkesson <thomas.akesson@simonsoft.se>
2019-03-27 11:20:17 +0100

    Introducing a runtime exception that explicitly carries a message to the end user.

---
+++ src/main/java/se/simonsoft/cms/item/MessageRuntimeException.java
@@ -0,0 +1,30 @@
+package se.simonsoft.cms.item;
+
+/**
+ * A runtime exception that explicitly carries a message to the end user.
+ * This exception should typically not be wrapped by other, less specific, exceptions.
+ * 
+ * This exception class might be useful for localization in the future.
+ */
+public class MessageRuntimeException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public MessageRuntimeException() {
+		super();
+	}
+
+	public MessageRuntimeException(String message) {
+		super(message);
+	}
+
+	public MessageRuntimeException(Throwable cause) {
+		super(cause);
+	}
+
+	public MessageRuntimeException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+
+}
