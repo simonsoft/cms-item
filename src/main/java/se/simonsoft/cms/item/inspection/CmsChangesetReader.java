bfcfa331249ac306dc3cbd0e816f76b63ed6377b
Thomas Åkesson <thomas.akesson@simonsoft.se>
2020-08-06 22:06:56 +0200

    #919 Removed deprecated methods, now clean per-repository services.

--- src/main/java/se/simonsoft/cms/item/inspection/CmsChangesetReader.java
+++ src/main/java/se/simonsoft/cms/item/inspection/CmsChangesetReader.java
@@ -21,32 +21,23 @@ import se.simonsoft.cms.item.events.change.CmsChangesetItem;
 
 /**
  * 
- * TODO this service should be per-repository instead, avoiding the need for passing around inspection and allowing different impls per repository
+ * #919 this service is now per-repository instead, avoiding the need for passing around inspection and allowing different impls per repository
  */
 public interface CmsChangesetReader {
 
 	/**
-	 * 
-	 * @param repository The repository to read
 	 * @param revision The changeset to produce
 	 * @return changeset at the revision, isOverwritten always set to false
-	 * @deprecated the service is per-repository, use {@link #read(RepoRevision)}
 	 */
-	CmsChangeset read(CmsRepositoryInspection repository, RepoRevision revision);
-	
 	CmsChangeset read(RepoRevision revision);
 	
 	/**
 	 * Useful when batch processing revisions to set a later revision, typically current HEAD,
 	 * to compare to in order to provide {@link CmsChangesetItem#isOverwritten()} information.
-	 * @param repository The repository to read
 	 * @param revision The changeset to produce
 	 * @param referenceRevision Larger than or equal to revision, normally identical in subsequent calls in the same batch
 	 * @return changeset at the revision, with isOverwritten set to true if referenceRevision contains a newer version of same item
-	 * @deprecated the service is per-repository, use {@link #read(RepoRevision, RepoRevision)}
 	 */
-	CmsChangeset read(CmsRepositoryInspection repository, RepoRevision revision, RepoRevision referenceRevision);	
-	
 	CmsChangeset read(RepoRevision revision, RepoRevision referenceRevision);
 	
 }
