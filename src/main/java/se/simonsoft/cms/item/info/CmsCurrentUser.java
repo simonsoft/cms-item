7139b52fa51535f9ae8be5b47b0ce1c5ae30b135
Thomas Åkesson <thomas.akesson@simonsoft.se>
2021-12-17 00:32:49 +0100

    Abstraction providing information about the current user for the current request. Replace authproxy.

---
+++ src/main/java/se/simonsoft/cms/item/info/CmsCurrentUser.java
@@ -0,0 +1,11 @@
+package se.simonsoft.cms.item.info;
+
+/**
+ * Abstraction providing information about the current user for the current request.
+ *
+ */
+public interface CmsCurrentUser {
+	
+	public String getUsername();
+
+}
