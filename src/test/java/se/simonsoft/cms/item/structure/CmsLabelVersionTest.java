ad2f0540e41028751fc91c1d069036e7bccde38d
Thomas Åkesson <thomas.akesson@simonsoft.se>
2022-05-04 17:18:11 +0200

    Test coverage investigating Semver 2.0.0 prerelease.

--- src/test/java/se/simonsoft/cms/item/structure/CmsLabelVersionTest.java
+++ src/test/java/se/simonsoft/cms/item/structure/CmsLabelVersionTest.java
@@ -17,6 +17,9 @@ package se.simonsoft.cms.item.structure;
 
 import static org.junit.Assert.*;
 
+import java.util.Iterator;
+import java.util.TreeSet;
+
 import org.junit.Test;
 
 public class CmsLabelVersionTest {
@@ -143,5 +146,91 @@ public class CmsLabelVersionTest {
 		assertFalse("string sort not desirable", l1.toString().compareTo(l2.toString()) < 1);
 		assertTrue("sortable gettor", l1.getLabelSort().compareTo(l2.getLabelSort()) < 1);
 		assertTrue("sortable objects", l1.compareTo(l2) < 1);
+	}
+
+	
+	@Test
+	public void testSemverPre1() { // Semver 2.0.0 introduced prerelease strings.
+		CmsLabelVersion l = new CmsLabelVersion("A.3.10-beta.11");
+		assertEquals("A.3.10-beta.11", l.getLabel());
+		//assertEquals("000000000A.0000000003.0000000010-beta.11", l.getLabelSort());
+		//assertEquals(3, l.getSegments().size());
+		assertEquals("A", l.getSegments().get(0));
+		assertEquals("3", l.getSegments().get(1));
+		//assertEquals("10", l.getSegments().get(2));
+		assertEquals("000000000A", l.getSegmentsSort().get(0));
+		assertEquals("0000000003", l.getSegmentsSort().get(1));
+		//assertEquals("0000000010", l.getSegmentsSort().get(2));
+		
+		// Release of the same Beta
+		CmsLabelVersion lR = new CmsLabelVersion("A.3.10");
+
+		TreeSet<CmsLabelVersion> set = new TreeSet<CmsLabelVersion>();
+		set.add(lR);
+		set.add(l);
+		Iterator<CmsLabelVersion> it = set.iterator();
+		assertEquals("A.3.10-beta.11", it.next().getLabel());
+		assertEquals("A.3.10", it.next().getLabel());
+		
+		
+		assertTrue("sortable label in this case", l.getLabel().compareTo(lR.getLabel()) < 1);
+		assertTrue("sortable gettor", l.getLabelSort().compareTo(lR.getLabelSort()) < 1);
+		assertTrue("sortable objects", l.compareTo(lR) < 1);
+	}
+	
+	@Test
+	public void testSemverPreSort1() { // Semver 2.0.0 introduced prerelease strings.
+		TreeSet<CmsLabelVersion> set = new TreeSet<CmsLabelVersion>();
+		// Sorting example from SemVer 2.0.0
+		// 1.0.0-alpha < 1.0.0-alpha.1 < 1.0.0-alpha.beta < 1.0.0-beta < 1.0.0-beta.2 < 1.0.0-beta.11 < 1.0.0-rc.1 < 1.0.0.
+		set.add(new CmsLabelVersion("1.0.5"));
+		set.add(new CmsLabelVersion("1.0.10"));
+		set.add(new CmsLabelVersion("1.0"));
+		set.add(new CmsLabelVersion("1.0.5-alpha"));
+		set.add(new CmsLabelVersion("1.0.5-alpha.1"));
+		set.add(new CmsLabelVersion("1.0.5-alpha.beta"));
+		set.add(new CmsLabelVersion("1.0.5-beta"));
+		set.add(new CmsLabelVersion("1.0.5-beta.2"));
+		set.add(new CmsLabelVersion("1.0.5-beta.11"));
+		set.add(new CmsLabelVersion("1.0.5-rc.1"));
+		// The following are CMS additions where we allow additional identifiers in the release version.
+		set.add(new CmsLabelVersion("1.0.5.0"));
+		set.add(new CmsLabelVersion("1.0.5.a"));
+		
+		Iterator<CmsLabelVersion> it = set.iterator();
+		assertEquals("1.0", it.next().getLabel());
+		assertEquals("1.0.5-alpha", it.next().getLabel());
+		assertEquals("1.0.5-alpha.1", it.next().getLabel());
+		assertEquals("1.0.5-alpha.beta", it.next().getLabel());
+		assertEquals("1.0.5-beta", it.next().getLabel());
+		assertEquals("1.0.5-beta.2", it.next().getLabel());
+		assertEquals("1.0.5-beta.11", it.next().getLabel());
+		assertEquals("1.0.5-rc.1", it.next().getLabel());
+		assertEquals("1.0.5", it.next().getLabel());
+		assertEquals("1.0.5.0", it.next().getLabel());
+		assertEquals("1.0.5.a", it.next().getLabel());
+	}
+	
+	@Test
+	public void testSortSeparators() {
+		// Need a special indicator for "end-of-string" sorting before . and 0 but after - (or its replacement)
+		// Last - becomes , or +
+		// End indicated by -
+		TreeSet<String> set = new TreeSet<String>();
+		set.add(".");
+		set.add(",");
+		set.add("-");
+		set.add("_");
+		set.add("+");
+		set.add(":");
+		set.add(";");
+		
+		Iterator<String> it = set.iterator();
+		assertEquals("+", it.next());
+		assertEquals("-", it.next());
+		assertEquals(".", it.next());
+		assertEquals(":", it.next());
+		assertEquals(";", it.next());
+		assertEquals("_", it.next());
 	}
 }
